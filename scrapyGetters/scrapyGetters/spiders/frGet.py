c061bd5a2f73dc24009c8021550b7ba308c249c6
JosephCarrino <giuseppe.carrino2@studio.unibo.it>
2021-10-03 13:45:43 +0200

    Added France 24

---
+++ scrapyGetters/scrapyGetters/spiders/frGet.py
@@ -0,0 +1,52 @@
+import scrapy
+from scrapy.http import HtmlResponse
+from scrapy import Selector
+from datetime import datetime
+import time
+import os
+import json
+import re
+
+class FrgetSpider(scrapy.Spider):
+    name = 'frGet'
+    urls= []
+    for i in range(1, 31):
+        urls.append("https://www.france24.com/en/archives/2021/09/" + str(i) + "-September-2021")
+    allowed_domains = ['https://www.france24.com/en']
+    start_urls = urls
+
+    def parse(self, response):
+        news= response.css(".o-archive-day__list").css("li")
+        titles= []
+        urls= []
+        raw_dates= []
+        dates= []
+        rankeds= []
+        act_date= datetime.strptime(response.url[45:], "%d-%B-%Y")
+        i= 0
+        for new in news:
+            titles.append(new.css("a::text").get())
+            urls.append("https://www.france24.com" + new.css("a::attr(href)").get())
+            raw_dates.append(act_date.strftime("%B %d, %Y"))
+            dates.append(act_date.strftime("%Y-%m-%d"))
+            rankeds.append(i)
+            i+=1
+        
+        edition= []
+        for item in zip(titles, raw_dates, dates, urls, rankeds):
+            scraped_info = {
+                'title': item[0],
+                'date_raw': item[1],
+                'date': item[2],
+                'url': item[3],
+                'content': "",
+                'ranked': item[4],
+                'placed': "First_Page",
+                'epoch': time.time()
+            }
+            edition.append(scraped_info)
+            
+        f= open("../../../collectedNews/FR/France24/" + str(edition[0]['date']) + ".json", "w")
+        json.dump(edition, f, indent= 4, ensure_ascii=False)
+        f.close()
+        
