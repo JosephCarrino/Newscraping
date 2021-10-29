50e4b171b0ea300a23147ab620ed66b067e1227a
Giuseppe Carrino <josephcarrinospam@gmail.com>
2021-10-29 14:00:05 +0200

    bbc_checkout

--- url_sed.py
+++ url_sed.py
@@ -7,4 +7,4 @@ for nation in os.listdir("collectedNews/"):
     for source in os.listdir("collectedNews/" + nation):
         for edition in os.listdir("collectedNews/" + nation + "/" + source):
             #print("collectedNews/" + nation + "/" + source + "/" + edition)
-            os.system("sed -i 's/url/news_url/' " + "collectedNews/" + nation + "/" + source + "/" + edition) 
+            os.system("sed -i 's/news_news_url/news_url/' " + "collectedNews/" + nation + "/" + source + "/" + edition) 
