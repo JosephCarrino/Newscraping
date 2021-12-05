3732314f3f182f275d79063670b13c0d63e3cc25
Giuseppe Carrino <giuseppe.carrino2@studio.unibo.it>
2021-12-05 18:14:45 +0100

    primi test Tagesschau

---
+++ testing.py
@@ -0,0 +1,14 @@
+import vcr
+import nytimesGet
+from urllib.request import urlopen
+
+f= open("nytKey.txt", "r+")
+key=f.read()
+f.close()
+
+import vcr
+from urllib.request import urlopen
+
+with vcr.use_cassette('fixtures/vcr_cassettes/synopsis.yaml'):
+    response = urlopen("https://www.tagesschau.de/multimedia/video/videoarchiv2.html").read()
+    assert b"viewB" in response
