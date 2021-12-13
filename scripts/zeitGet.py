5e35e30c1758909aa4f3cdd7a3e8d3224b19d913
Gioele Barabucci <gioele.barabucci@ntnu.no>
2021-12-13 12:22:50 +0100

    scripts/zeitGet: Realign with NYT script

--- scripts/zeitGet.py
+++ scripts/zeitGet.py
@@ -1,24 +1,21 @@
 import json
 import urllib.request
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
-import os
-import re
+from os import path
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
-def sorted_nicely( l ): 
-    """ Sort the given iterable in the way that humans expect.""" 
-    convert = lambda text: int(text) if text.isdigit() else text 
-    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
-    return sorted(l, key = alphanum_key)
+SCRIPTS_DIR = path.dirname(__file__)
+PROJ_DIR = f"{SCRIPTS_DIR}/../"
+
 
 def main():
-    f= open("keys/zeitKey.txt", "r+")
-    key=f.read()
+    key = read_key("zeit")
 
-    #Insert here your ZeitAPI key
-    #key= ""
+    now = datetime.now()
+    now_s = now.strftime("%Y-%m-%dT%H.%M.%S")
+    now_epoch = (now - datetime(1970, 1, 1)) / timedelta(seconds=1)
 
     url= "https://api.zeit.de/content?fields=title+subtitle+href+release_date+&limit=50"
     hdr= { 'X-Authorization' : key }
@@ -28,20 +25,13 @@ def main():
     objs= json.loads(toGet.read().decode("utf-8"))
     news= objs['matches']
 
-    #files= sorted_nicely(os.listdir("zeitDE"))
-    #if len(files) == 0:
-        #j= 0
-    #else:
-        #j= len(files)
-
     titles= []
     raw_dates= []
     dates= []
     urls= []
     contents= []
 
-    top= True
-    for new in news: 
+    for new in news:
         titles.append(new['title'])
         ddate= datetime.strptime(new['release_date'][0:10], "%Y-%m-%d")
         raw_dates.append(ddate.strftime("%B %d, %Y"))
@@ -50,7 +40,6 @@ def main():
         contents.append(new['subtitle'])
 
     i= 0
-    toDump= True
     edition= []
     for item in zip(titles, raw_dates, dates, urls, contents):
         scraped_info = {
@@ -63,27 +52,27 @@ def main():
             'ranked': i,
             'epoch': time.time()
         }
-        #for lastNew in files:
-            #if lastNew != "":
-                #f= open("zeitDE/" + lastNew, "r+")
-                #searchin= json.load(f)
-                #if searchin['url'] == scraped_info['url']:
-                    #f.close()
-                    #toDump= False
         i+=1
         if(i <= 20):
             edition.append(scraped_info)
-    now = datetime.now().strftime("%Y-%m-%dT%H.%M.%S")
-    if toDump:
-        my_f = str(now) + "E" + str(time.time())
-        f= open("collectedNews/flow/DE/Zeit/" + my_f + ".json", "w")
-        json.dump(edition, f, indent= 4)
-        f.close()
-        f= open("collectedNews/flow/DE/Zeit/" + my_f + ".json", "a")
+
+    base_name = f"{now_s}E{now_epoch}.json"
+    scraped_data_dir = f"{PROJ_DIR}/collectedNews/flow/DE/Zeit/"
+    scraped_data_filepath = f"{scraped_data_dir}/{base_name}"
+    with open(scraped_data_filepath, "w") as f:
+        json.dump(edition, f, indent=4, ensure_ascii=False)
         f.write("\n")
-        f.close()
-        i+=1
-        #j+=1
+
+
+def read_key(key_name: str) -> str:
+    key_dir = f"{PROJ_DIR}/keys"
+
+    with open(f"{key_dir}/{key_name}Key.txt", "r") as f:
+        key = f.read()
+    key = key.strip()
+
+    return key
+
 
 if __name__ == "__main__":
     main()          
