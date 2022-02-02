d0b3a2e66af081890309203699da90912690cef1
JosephCarrino <josephcarrinospam@gmail.com>
2022-02-02 15:42:08 +0100

    20Get: refatored

--- scrapyGetters/scrapyGetters/spiders/20Get.py
+++ scrapyGetters/scrapyGetters/spiders/20Get.py
@@ -1,33 +1,27 @@
+#!/usr/bin/env python
+
 import scrapy
 from scrapy.http import HtmlResponse
 from scrapy import Selector
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
-import os
+from os import path
 import json
-import re
 from scrapy.crawler import CrawlerProcess
 import vcr
 from urllib.request import urlopen
 
-test_dir = ""
-
-#sort i've found o StackOverflow, if I didn't use this I couldn't find the "last saved news" below
-#credits to Mark Byers
-def sorted_nicely( l ): 
-    """ Sort the given iterable in the way that humans expect.""" 
-    convert = lambda text: int(text) if text.isdigit() else text 
-    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
-    return sorted(l, key = alphanum_key)
-
+SCRIPTS_DIR = path.dirname(__file__)
+PROJ_DIR = f"{SCRIPTS_DIR}/../../../"
+ARCH_URL = "https://www.tagesschau.de/multimedia/video/videoarchiv2.html"
+BASE_URL = "https://www.tagesschau.de"
 
 class A20getSpider(scrapy.Spider):
     name = '20Get'
-    allowed_domains = ['www.tagesschau.de/multimedia/video/videoarchiv2.html']
-    start_urls = ['https://www.tagesschau.de/multimedia/video/videoarchiv2.html']
+    allowed_domains = [ARCH_URL]
+    start_urls = [ARCH_URL]
 
     def parse(self, response):
-        base_url="https://www.tagesschau.de"
         boxes = response.css(".viewB")
         toRet= []
         for box in boxes:
@@ -36,23 +30,17 @@ class A20getSpider(scrapy.Spider):
                 if box.css(".dachzeile::text").get()[11:16] == "20:00":
                     toRet.append(box)   
         
-        #lastNew = ""
-        #files= sorted_nicely(os.listdir("../../../DE"))
-        #if len(files) == 0:
-            #j= 0
-        #else:
-            #lastNew= files[len(files)-1]
-            #j= len(files)
-        
+        now = datetime.now()
+        now_s = now.strftime("%Y-%m-%dT%H.%M.%S")
+        now_epoch = (now - datetime(1970, 1, 1)) / timedelta(seconds=1)
                              
         
         for box in toRet:
-            toDump= True
             findTitle= box.css(".teasertext")
             titles= findTitle.css('a::text').get().split(",")
             contents= ""
             findUrl= box.css(".headline")
-            urls= base_url + findUrl.xpath('.//a').css('::attr(href)').get()
+            urls= BASE_URL + findUrl.xpath('.//a').css('::attr(href)').get()
             findDate= str(box.css("p::text").get())[0:10]
             dates= datetime.strptime(findDate, "%d.%m.%Y").strftime("%B %d, %Y")
             edition= []
@@ -68,29 +56,19 @@ class A20getSpider(scrapy.Spider):
                     'ranked': str(i),
                     'epoch': time.time()
                 }
-                #if i == 0:
-                    #if lastNew != "":
-                        #f= open("../../../DE/" + lastNew, "r+")
-                        #searchin= json.load(f)
-                        #if searchin[0]['date'] >= scraped_info['date']:
-                            #f.close()
-                            #toDump= False
-                            #break
-                #else:
-                    #scraped_info['title']= title[1:len(title)]
                 i+=1
                 edition.append(scraped_info)
-            print(edition)
-            if toDump:
-                f= open("../../../collectedNews/edition/DE/Tagesschau/" + str(scraped_info['date']) + ".json", "w")
-                json.dump(edition, f, indent= 4, ensure_ascii=False)
-                f.close()
-                f= open("../../../collectedNews/edition/DE/Tagesschau/" + str(scraped_info['date']) + ".json", "a")
+
+            base_name = f"{str(scraped_info['date'])}.json"
+            scraped_data_dir = f"{PROJ_DIR}/collectedNews/edition/DE/Tagesschau"
+            scraped_data_filepath = f"{scraped_data_dir}/{base_name}"
+            with open(scraped_data_filepath, "w") as f:
+                json.dump(edition, f, indent=4, ensure_ascii=False)
                 f.write("\n")
-                f.close()
-                #j+=1
-                global testdir
-                testdir = "../../../collectedNews/edition/DE/Tagesschau/" + str(scraped_info['date']) + ".json"
+            
+            global testdir
+            testdir = f"{scraped_data_filepath}"
+
 
          
 if __name__ == "__main__":
@@ -104,13 +82,11 @@ if __name__ == "__main__":
     response = json.load(f)
     f.close()
     with vcr.use_cassette('fixtures/vcr_cassettes/Tagesschau.yaml'):
-        myres = urlopen("https://www.tagesschau.de/multimedia/video/videoarchiv2.html").read()
+        myres = urlopen(ARCH_URL).read()
         if len(myres) > 0:
             assert 0 < len(response)  
             assert response[0]['title'] is not None 
             for thisresp in response:  
                 assert str.encode(thisresp['title']) in myres  
                 assert thisresp['url_news'] is not None
-                #curr_url = urlopen(thisresp['url_news']).read()
-                #assert str.encode(thisresp['title']) in curr_url
             
