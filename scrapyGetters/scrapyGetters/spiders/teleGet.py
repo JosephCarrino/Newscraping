7dcebbd2a9d58e44b24d112e16d510dec82264fe
JosephCarrino <josephcarrinospam@gmail.com>
2022-02-02 17:08:46 +0100

    teleGet: refactored

--- scrapyGetters/scrapyGetters/spiders/teleGet.py
+++ scrapyGetters/scrapyGetters/spiders/teleGet.py
@@ -1,20 +1,23 @@
+#!/usr/bin/env python
+
 import scrapy
 import json
-import re
-import os
-from datetime import datetime
+from os import path
+from datetime import datetime, timedelta
 from datetime import date
 import time
 import urllib.request
 
-#sort i've found o StackOverflow, if I didn't use this I couldn't find the "last saved news" below
-#credits to Mark Byers
-def sorted_nicely( l ): 
-    """ Sort the given iterable in the way that humans expect.""" 
-    convert = lambda text: int(text) if text.isdigit() else text 
-    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
-    return sorted(l, key = alphanum_key)
-
+SCRIPTS_DIR = path.dirname(__file__)
+PROJ_DIR = f"{SCRIPTS_DIR}/../../../"
+BASE_URL = f"www.servizitelevideo.rai.it/televideo/pub"
+CATE_URL = f"http://www.servizitelevideo.rai.it/televideo/pub/solotesto.jsp?categoria="
+ARCH_URLS = [
+    f"{CATE_URL}Prima&pagina=103",
+    f"{CATE_URL}Politica&pagina=120",
+    f"{CATE_URL}Politica&pagina=130",
+    f"{CATE_URL}Dal%20Mondo&pagina=150"
+]
 
 class TelegetSpider(scrapy.Spider):
     
@@ -29,36 +32,12 @@ class TelegetSpider(scrapy.Spider):
     base_url= "https://www.servizitelevideo.rai.it/televideo/pub/solotesto.jsp?pagina="
     
     name = 'teleGet'
-    allowed_domains = ['www.servizitelevideo.rai.it/televideo/pub']
-    start_urls = ['http://www.servizitelevideo.rai.it/televideo/pub/solotesto.jsp?categoria=Prima&pagina=103',
-                 'https://www.servizitelevideo.rai.it/televideo/pub/solotesto.jsp?categoria=Politica&pagina=120',
-                 'https://www.servizitelevideo.rai.it/televideo/pub/solotesto.jsp?categoria=Politica&pagina=130',
-                 'https://www.servizitelevideo.rai.it/televideo/pub/solotesto.jsp?categoria=Dal%20Mondo&pagina=150'
-                 ]
+    allowed_domains = [BASE_URL]
+    start_urls = ARCH_URLS
     
     def stringFormat(self, s):
         return s.replace('\n', ' ').replace('\\', '').replace('  ', ' ').strip()
-    
-    #Per ora non usato, ma vorrei riuscire a sistemare il problema delle librerie che sovrascrivono urlopen
-    def deepParse(toParse, subPage= 2):
-        toGet= urllib.request.urlopen(toParse)
-        newData= toGet.read().decode("utf8")
-        #cerco l'inizio
-        start= newData.find("<pre")
-        start= newData.find(">", start)
-        end=newData.find("<", start)
-        #lo formatto decentemente
-        toRet= stringFormat(newData[start+1:end])
-        try:
-            #se mi accorgo che ci sono più pagine, ottengo anche queste
-            if toRet[1] == "/":
-                page= toParse[len(toParse)-4:len(toParse)]
-                newPage = "https://www.servizitelevideo.rai.it/televideo/pub/solotesto.jsp?regione=&pagina" + page + "&sottopagina=0" + str(subPage)
-                toRet += deepParse(newPage, subPage+1)
-        except:
-            pass
-        return toRet
-    
+
     def parse(self, response):
         content= response.css("pre")
         titleString= content.css("::text").getall()
@@ -69,14 +48,9 @@ class TelegetSpider(scrapy.Spider):
         placed= []
         ranked= []
         
-        #lastNew = ""
-        #files= sorted_nicely(os.listdir("../../../collectedNews/IT/Televideo"))
-        #if len(files) == 0:
-            #j= 0
-        #else:
-           # lastNew= files[len(files)-1]
-            #j= len(files)
-        
+        now = datetime.now()
+        now_s = now.strftime("%Y-%m-%dT%H.%M.%S")
+        now_epoch = (now - datetime(1970, 1, 1)) / timedelta(seconds=1)
        
         edition= []
         i= 0
@@ -94,7 +68,7 @@ class TelegetSpider(scrapy.Spider):
                     placed.append(self.cate_conv[url[len(url)-2]])
             i+= 1
         i= 0
-        toDump= True
+
         for item in zip(titles, urls, contents, ranked, placed):
             scraped_info = {
                 'title': item[0],
@@ -107,23 +81,13 @@ class TelegetSpider(scrapy.Spider):
                 'placed': item[4],
                 'epoch': time.time()
             }
-            #if i == 0:
-                #for lastNew in files:
-                    #if lastNew != "":
-                        #f= open("../../../collectedNews/IT/Televideo/" + lastNew, "r+")
-                        #searchin= json.load(f)
-                        #if searchin[0]['url'] == scraped_info['url']:
-                            #f.close()
-                            #toDump= False
-                            #break
             i+=1
             edition.append(scraped_info)
-        now = datetime.now().strftime("%Y-%m-%dT%H.%M.%S")
-        if toDump:
-            my_f = str(now) + "E" + str(time.time())
-            f= open("../../../collectedNews/flow/IT/Televideo/" + my_f + ".json", "w")
+        
+        base_name = f"{now_s}E{now_epoch}.json"
+        scraped_data_dir = f"{PROJ_DIR}/collectedNews/flow/IT/Televideo"
+        scraped_data_filepath = f"{scraped_data_dir}/{base_name}"
+        with open(scraped_data_filepath, "w") as f:
             json.dump(edition, f, indent= 4, ensure_ascii=False)
-            f.close()
-            f= open("../../../collectedNews/flow/IT/Televideo/" + my_f + ".json", "a")
             f.write("\n")
-            f.close()
+
