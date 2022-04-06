9af89b1aafeed580c5be38888eff06b8a220fc8f
JosephCarrino <josephcarrinospam@gmail.com>
2022-04-06 10:47:59 +0200

    flowscrape.sh: added new scrapers.

--- scrapyGetters/scrapyGetters/spiders/flowscrape.sh
+++ scrapyGetters/scrapyGetters/spiders/flowscrape.sh
@@ -3,6 +3,10 @@ PATH=$PATH:/home/students/giuseppe.carrino2/.local/bin
 export PATH
 scrapy crawl teleGet
 scrapy crawl postScrape
+scrapy crawl dwGet
+scrapy crawl abcGet
+scrapy crawl cnnGEt
+scrapy crawl fr24rssGet
 cd ../../..
 git add .
 git commit -m "checkout"
